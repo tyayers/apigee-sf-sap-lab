4a1c3253e8fc2d125fb24a45344f8047253fa5af
Tyler Ayers <tyayers@google.com>
2021-02-12 14:05:41 +0000

    updated proxy

--- apigee-proxy/apiproxy/resources/jsc/beautify-json.js
+++ apigee-proxy/apiproxy/resources/jsc/beautify-json.js
@@ -1,3 +1,38 @@
-var body = response.content.asJSON;
+if (context.proxyResponse.content) {
+    var body = JSON.parse(context.proxyResponse.content);
+    var resource = context.getVariable("record.resource");
+    
+    if (body.d) {
+        var newBody = {};
+        
+        if (body.d.results && !newBody[resource])
+            newBody[resource] = [];
+                
+        if (body.d.results) {
+            for (i=0; i<body.d.results.length; i++) {
+                var record = body.d.results[i];
+                
+                newBody[resource].push(convertObject(record));
+            }
+        }
+        else {
+            newBody = convertObject(body.d);
+        }
+        
+        context.proxyResponse.content = JSON.stringify(newBody);
+    }
+}
 
-var newBody = {};
+function convertObject(inputObj) {
+    var result = {};
+    
+    for (var prop in inputObj) {
+
+        var myVar = inputObj[prop];
+
+        if ((typeof myVar === 'string' || myVar instanceof String) && myVar !== "")
+            result[prop] = inputObj[prop];        
+    }
+    
+    return result;
+}
